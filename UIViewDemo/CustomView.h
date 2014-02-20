4ef3a9725ab4eb7ce3dc9ec0c14cdf38e3bb0e74
David Kraft <kraftd@goldmail.etsu.edu>
2014-02-19 22:00:21 -0500

    First Commit

---
+++ UIViewDemo/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  UIViewDemo
+//
+//  Created by David Kraft on 2/19/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ UIViewDemo/CustomView.m
@@ -0,0 +1,31 @@
+//
+//  CustomView.m
+//  UIViewDemo
+//
+//  Created by David Kraft on 2/19/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
